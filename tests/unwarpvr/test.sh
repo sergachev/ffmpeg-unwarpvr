6688342286e00dce3dc8b075d0f62decef47653e
eVRydayVR <eVRydayVR@gmail.com>
2014-09-25 03:54:02 -0700

    Get building in Windows, modify tests to create video with yuv420p, which allows them to be loaded directly in Premiere Pro

--- tests/unwarpvr/test.sh
+++ tests/unwarpvr/test.sh
@@ -5,5 +5,5 @@ pushd ../.. && make ffmpeg && popd
 ../../ffmpeg -y -i lava_clip_short.mp4 -vf unwarpvr=2364:1461 -ss 0 -vframes 1 -f image2 output_full.png
 ../../ffmpeg -y -i lava_clip_short.mp4 -vf unwarpvr=2500:1600 -ss 0 -vframes 1 -f image2 output_big.png
 ../../ffmpeg -y -i lava_clip_short.mp4 -vf unwarpvr=1920:1080 -ss 0 -vframes 1 -f image2 output_cropped.png
-../../ffmpeg -y -i lava_clip_short.mp4 -vf unwarpvr=1920:1080 -c:a copy -c:v libx264 -crf 14 output_cropped.mp4
-../../ffmpeg -y -i lava_clip_short.mp4 -vf unwarpvr=2364:1461 -c:a copy -c:v libx264 -crf 14 output_full.mp4
+../../ffmpeg -y -i lava_clip_short.mp4 -vf unwarpvr=1920:1080 -c:a copy -c:v libx264 -crf 18 -pix_fmt yuv420p output_cropped.mp4
+../../ffmpeg -y -i lava_clip_short.mp4 -vf unwarpvr=2364:1462 -c:a copy -c:v libx264 -crf 18 -pix_fmt yuv420p output_full.mp4
